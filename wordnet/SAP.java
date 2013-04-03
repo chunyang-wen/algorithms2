834ba933e8b31435e3f9422037e2fd8c1d11b0e2
Billy Schwartz <wkschwartz@gmail.com>
2013-04-03 01:30:52 -0400

    Implement SAP with straightforward technique suggested by instructor

--- wordnet/SAP.java
+++ wordnet/SAP.java
@@ -8,8 +8,6 @@
  *
  ************************************************************************/
 
-import java.lang.IndexOutOfBoundsException;
-
 /**
  * Instances of <code>SAP</code> calculate shortest ancestral paths between two
  * verticies or two sets of verticies in a given digraph.
@@ -39,21 +37,77 @@ public class SAP {
 
 	// length of shortest ancestral path between v and w; -1 if no such path
 	public int length(int v, int w) {
+		BreadthFirstDirectedPaths pv = new BreadthFirstDirectedPaths(g, v);
+		if (pv.hasPathTo(w))
+			return pv.distTo(w);
+		BreadthFirstDirectedPaths pw = new BreadthFirstDirectedPaths(g, w);
+		int min, dist;
+		min = -1;
+		for (int node = 0; node < g.V(); node++) {
+			if (pv.hasPathTo(node) && pw.hasPathTo(node)) {
+				dist = pv.distTo(node) + pw.distTo(node);
+				if (min < 0 || dist < min)
+					min = dist;
+			}
+		}
+		return min;
 	}
 
 	// a common ancestor of v and w that participates in a shortest ancestral
 	// path; -1 if no such path
 	public int ancestor(int v, int w) {
+		BreadthFirstDirectedPaths pv = new BreadthFirstDirectedPaths(g, v);
+		if (pv.hasPathTo(w))
+			return pv.distTo(w);
+		BreadthFirstDirectedPaths pw = new BreadthFirstDirectedPaths(g, w);
+		int min, dist, argmin;
+		argmin = min = -1;
+		for (int node = 0; node < g.V(); node++) {
+			if (pv.hasPathTo(node) && pw.hasPathTo(node)) {
+				dist = pv.distTo(node) + pw.distTo(node);
+				if (min < 0 || dist < min) {
+					min = dist;
+					argmin = node;
+				}
+			}
+		}
+		return argmin;
 	}
 
 	// length of shortest ancestral path between any vertex in v and any vertex
 	// in w; -1 if no such path. Iterables must contain at least one int.
 	public int length(Iterable<Integer> v, Iterable<Integer> w) {
+		BreadthFirstDirectedPaths pv = new BreadthFirstDirectedPaths(g, v);
+		BreadthFirstDirectedPaths pw = new BreadthFirstDirectedPaths(g, w);
+		int min, dist;
+		min = -1;
+		for (int node = 0; node < g.V(); node++) {
+			if (pv.hasPathTo(node) && pw.hasPathTo(node)) {
+				dist = pv.distTo(node) + pw.distTo(node);
+				if (min < 0 || dist < min)
+					min = dist;
+			}
+		}
+		return min;
 	}
 
 	// a common ancestor that participates in shortest ancestral path; -1 if no
 	// such path. Iterables must contain at least one int.
 	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+		BreadthFirstDirectedPaths pv = new BreadthFirstDirectedPaths(g, v);
+		BreadthFirstDirectedPaths pw = new BreadthFirstDirectedPaths(g, w);
+		int min, dist, argmin;
+		argmin = min = -1;
+		for (int node = 0; node < g.V(); node++) {
+			if (pv.hasPathTo(node) && pw.hasPathTo(node)) {
+				dist = pv.distTo(node) + pw.distTo(node);
+				if (min < 0 || dist < min) {
+					min = dist;
+					argmin = node;
+				}
+			}
+		}
+		return argmin;
 	}
 
 	/**
