10ca1c49b6f313f59269ef6b64edca3e17a443f4
William Schwartz <wkschwartz@gmail.com>
2013-04-06 13:54:00 -0400

    Change name of attribute from picture to pic

--- seamcarving/SeamCarver.java
+++ seamcarving/SeamCarver.java
@@ -5,17 +5,17 @@
  */
 public class SeamCarver {
 	private static final double BORDER_ENERGY = 3 * (255 * 255);
-	private Picture picture;
-	public SeamCarver(Picture picture) { this.picture = picture; }
+	private Picture pic;
+	public SeamCarver(Picture picture) { this.pic = picture; }
 
-	// current picture
-	public Picture picture() { return picture; }
+	// Copy of current picture
+	public Picture picture() { return pic; }
 
 	// width of current picture
-	public int width() { return picture.width(); }
+	public int width() { return pic.width(); }
 
 	// height of current picture
-	public int height() { return picture.height(); }
+	public int height() { return pic.height(); }
 
 	// Find the square color gradient in one dimension
 	private int gradient(java.awt.Color a, java.awt.Color b) {
@@ -29,8 +29,8 @@ public class SeamCarver {
 	public double energy(int x, int y) {
 		if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1)
 			return BORDER_ENERGY;
-		return gradient(picture.get(x - 1, y), picture.get(x + 1, y)) +
-		       gradient(picture.get(x, y - 1), picture.get(x, y + 1));
+		return gradient(pic.get(x - 1, y), pic.get(x + 1, y)) +
+		       gradient(pic.get(x, y - 1), pic.get(x, y + 1));
 	}
 
 	// sequence of indices for horizontal seam
@@ -53,12 +53,12 @@ public class SeamCarver {
 		for (int col = 0; col < width(); col++) {
 			for (int row = 0; row < height() - 1; row++) {
 				if (row < a[col])
-					p.set(col, row, picture.get(col, row));
+					p.set(col, row, pic.get(col, row));
 				else
-					p.set(col, row, picture.get(col, row + 1));
+					p.set(col, row, pic.get(col, row + 1));
 			}
 		}
-		picture = p;
+		pic = p;
 	}
 
 
@@ -76,11 +76,11 @@ public class SeamCarver {
 		for (int row = 0; row < height(); row++) {
 			for (int col = 0; col < width() - 1; col++) {
 				if (col < a[row])
-					p.set(col, row, picture.get(col, row));
+					p.set(col, row, pic.get(col, row));
 				else
-					p.set(col, row, picture.get(col + 1, row));
+					p.set(col, row, pic.get(col + 1, row));
 			}
 		}
-		picture = p;
+		pic = p;
 	}
 }
