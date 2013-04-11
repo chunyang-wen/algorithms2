b98815e921f2a60ecc7084098380f527295b1a18
Billy Schwartz <wkschwartz@gmail.com>
2013-04-10 23:27:06 -0400

    Off-by-one in checking indicies passed to energy()

--- seamcarving/SeamCarver.java
+++ seamcarving/SeamCarver.java
@@ -28,7 +28,7 @@ public class SeamCarver {
 
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		if (x < 0 || x >= width() || y < 0 || y > height()) {
+		if (x < 0 || x >= width() || y < 0 || y >= height()) {
 			String msg = Integer.toString(x) + ", " + Integer.toString(y);
 			throw new IndexOutOfBoundsException(msg);
 		}
